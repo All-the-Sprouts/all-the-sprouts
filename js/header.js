0f086fbb5958b6b409c81a0e0608913311e1924c
Tact <69988786+ItsTact@users.noreply.github.com>
2024-01-01 11:04:11 -0500

    push for the new year

---
+++ js/header.js
@@ -0,0 +1,8 @@
+fetch('../ui/header.html')
+.then(res => res.text())
+.then(text => {
+    let oldelem = document.querySelector("script#replace_with_header");
+    let newelem = document.createElement("div");
+    newelem.innerHTML = text;
+    oldelem.parentNode.replaceChild(newelem,oldelem);
+})
